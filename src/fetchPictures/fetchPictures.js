a5ed7dc270ee7a8b78cd2a34b669c0318d2a41c4
AndreyKapus <kapustnikov@ukr.net>
2023-03-18 13:30:26 +0200

    draw

--- src/fetchPictures/fetchPictures.js
+++ src/fetchPictures/fetchPictures.js
@@ -2,9 +2,12 @@
  const API_KEY = "29432159-064ba5645d6ae7f18ff2bb6d2";
 
  export function fetchPics(data) {
-  fetch(`${API}/?key=${API_KEY}&q=${data}&image_type=photo&orientation=horizontal&safesearch-true`).then(res => {
+  fetch(`${API}/?key=${API_KEY}&q=${data}&image_type=photo&orientation=horizontal&safesearch-true`)
+  .then((res) => {
+    if(!res.ok) {
+      throw new Error(res.statusText)
+    }
     return res.json()
   })
  };
 
- 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,43 @@ import { fetchPics } from "./fetchPictures/fetchPictures";
 
 const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('input[name=searchQuery]');
+const bodyEl = document.querySelector('.body')
 
-formEl.addEventListener('submit', onFormSubmit)
+
 
 function onFormSubmit(e) {
   e.preventDefault();
   const inputQuery = e.currentTarget.elements.searchQuery.value;
 
 
-  fetchPics(inputQuery)
+  fetchPics(inputQuery).then(data => drawPictures(data)).catch(console.log(error))
+
 }
 
+function drawPictures(data) {
+  const {hits} = data;
 
+const markup = hits.map(({id, webformatURL, largeImageURL, tags, likes, comments, downloads, views}) => {
+      return `
+      <div>
+        <ul>
+          <li>
+            <img src=${webformatURL} width='300px'/>
+            <div>${likes}</div>
+            <div>${tags}</div>
+            </li>
+        </ul>
+      </div>
+      `;
+  }).join('');
+  bodyEl.insertAdjacentHTML('beforeend', markup)
+}
 
+// function checkData(data) {
+//   if(data.lenght >= 1) {
+//     drawPictures(data)
+//   }
+//   drawPictures(data)
+// }
+
+formEl.addEventListener('submit', onFormSubmit)
